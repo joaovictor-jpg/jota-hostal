99c46511c83e3b1e3c09cdeadf5ec432c20542cc
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-05-04 18:07:45 -0300

    feat: created eureka service

---
+++ service/src/test/java/br/com/jota/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jota.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
